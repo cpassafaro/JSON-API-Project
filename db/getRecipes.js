0566350e778902d3d4329eab2cc888ed3075bc2b
christinapassafaro <christinapassafaro@gmail.com>
2020-11-05 10:10:22 -0500

    added more to db

--- db/getRecipes.js
+++ db/getRecipes.js
@@ -3,14 +3,14 @@ const unirest = require('unirest');
 const API_KEY = "c40a9f3979dc415c9bedb13f41fb1449"
 const fs = require('fs');
 
-const url = 'https://api.spoonacular.com/recipes/informationBulk?ids=645348,645354,648279,648097,654327,642559,639557,1449643,651426,651407,663121,663144,663126,649048,649056,649062,647830,647799,647867,649300,653234,634751,649381,1098392,647675,632003,716298,653275,635263,636736,636743&apiKey=c40a9f3979dc415c9bedb13f41fb1449';
+const url = 'https://api.spoonacular.com/recipes/informationBulk?ids=715446,794349,782601,716426,715594,715497&apiKey=c40a9f3979dc415c9bedb13f41fb1449';
 
 unirest.get(url)
     .header('X-RapidAPI-Key', API_KEY)
     .end(res => {
         if(res.status == 200){
             let jsonRecipes = JSON.stringify(res.body)
-            fs.writeFile('data.json', jsonRecipes, err =>{
+            fs.appendFile('data.json', jsonRecipes, err =>{
                 if(err){
                     console.log(err)
                 }else{
@@ -19,3 +19,19 @@ unirest.get(url)
             })
         }
     })
+
+
+//looking up more if you want to add
+// unirest.get('https://api.spoonacular.com/recipes/complexSearch?genre=Korean&apiKey=c40a9f3979dc415c9bedb13f41fb1449')
+//     .header('X-RapidAPI-Key', API_KEY)
+//     .end(res => {
+//         // console.log(res)
+//         let jsonRecipes = JSON.stringify(res.body)
+//         fs.writeFile('experimental.json', jsonRecipes, err => {
+//             if(err){
+//                 console.log(err)
+//             }else{
+//                 console.log('file created')
+//             }
+//         })
+//     })
